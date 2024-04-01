755a8d0d63550312d52fc1e4e379da12139c4e1f
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-04-01 23:52:53 +0700

    chore: add description

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,10 +1,14 @@
 import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Personal prize redemption system using Telegram Bot"
+        />
         <title>Redeem code for prize from Kalwabed</title>
         <link rel="stylesheet" href="/styles.css" />
         <link rel="stylesheet" href="/neat.css" />
